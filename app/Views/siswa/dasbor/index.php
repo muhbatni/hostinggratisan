3398dcedca9667830150cdfc85b5e11a6b902f12
Batni <ibat366@gmail.com>
2025-05-03 17:31:43 +0700

    chore: bahan web hosting

---
+++ app/Views/siswa/dasbor/index.php
@@ -0,0 +1,4 @@
+<div class="callout callout-success">
+	Hai <strong><?php echo Session()->get('nama_siswa') ?></strong>, 
+	Selamat datang di <strong><?php echo $this->website->namaweb() ?></strong>
+</div>
